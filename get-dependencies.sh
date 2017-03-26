38e132fbcded69e6d9500fec11261ed1726c0a65
juju4 <juju4@users.noreply.github.com>
2017-03-25 20:35:35 -0400

    enable https in default play with self-signed certificate, review backup script + monit configuration + serverspec tests + rsyslog stop

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,9 +11,11 @@ fi
 
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/geerlingguy.nginx ] && git clone https://github.com/geerlingguy/ansible-role-nginx.git $rolesdir/geerlingguy.nginx
-[ ! -d $rolesdir/geerlingguy.jenkins ] && git clone https://github.com/geerlingguy/ansible-role-jenkins.git $rolesdir/geerlingguy.jenkins
+#[ ! -d $rolesdir/geerlingguy.jenkins ] && git clone https://github.com/geerlingguy/ansible-role-jenkins.git $rolesdir/geerlingguy.jenkins
+[ ! -d $rolesdir/geerlingguy.jenkins ] && git clone https://github.com/juju4/ansible-role-jenkins.git $rolesdir/geerlingguy.jenkins
 [ ! -d $rolesdir/tersmitten.nodejs ] && git clone https://github.com/Oefenweb/ansible-nodejs.git $rolesdir/tersmitten.nodejs
 [ ! -d $rolesdir/juju4.w3af ] && git clone https://github.com/juju4/ansible-w3af $rolesdir/juju4.w3af
+[ ! -d $rolesdir/juju4.harden-nginx ] && git clone https://github.com/juju4/ansible-harden-nginx $rolesdir/juju4.harden-nginx
 ## to configure ssh auth to jenkins
 [ ! -d $rolesdir/cmprescott.xml ] && git clone https://github.com/cmprescott/ansible-xml.git $rolesdir/cmprescott.xml
 ## galaxy naming: kitchen fails to transfer symlink folder
