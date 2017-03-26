f11c75dd0adfc072069ec902e7f3974a116fdb49
juju4 <juju4@users.noreply.github.com>
2017-03-25 22:25:13 -0400

    fix serverspec spec with https as default

--- test/integration/default/serverspec/jenkins_curl_spec.rb
+++ test/integration/default/serverspec/jenkins_curl_spec.rb
@@ -3,19 +3,22 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+#jenkins_url = 'http://localhost:9091/'
+jenkins_url = 'https://localhost:9091/'
+curl_arg = '-vkL'
 
-describe command('curl -v http://localhost:9091/') do
+describe command("curl #{curl_arg} #{jenkins_url}") do
 #  its('stdout') { should match /<meta http-equiv='refresh' content='1;url=\/login?from=%2F'\/>/ }
   its('stdout') { should match /You are authenticated as: anonymous/ }
   its('stderr') { should match "HTTP/1.1 403 Forbidden" }
 end
 
 ## plugins test
-describe command('curl -vkL http://localhost:9091/git/notifyCommit?url=') do
+describe command("curl #{curl_arg} #{jenkins_url}/git/notifyCommit?url=") do
   its('stdout') { should match "java.lang.Exception: Illegal URL:" }
   its('stdout') { should_not match "404 Not Found" }
 end
-describe command('curl -vkL http://localhost:9091/github-webhook/') do
+describe command("curl #{curl_arg} #{jenkins_url}/github-webhook/") do
 #  its('stdout') { should match "java.lang.Exception: Method POST required" }
   its('stdout') { should_not match "404 Not Found" }
   its('stdout') { should match "Authentication required" }
