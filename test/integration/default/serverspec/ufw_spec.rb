0816ce7bf47f651b4ee1a53f2b1df4b1ba09092a
juju4 <juju4@users.noreply.github.com>
2018-07-02 11:28:04 -0400

    fix some serverspec testing

--- test/integration/default/serverspec/ufw_spec.rb
+++ test/integration/default/serverspec/ufw_spec.rb
@@ -8,7 +8,7 @@ describe command('host www.google.com') do
   its('stdout') { should match /www.google.com has address / }
 end
 describe command('curl -v https://www.google.com/') do
-  its('stdout') { should match /Search the world's information/ }
+  its('stdout') { should match /<title>Google<\/title>/ }
   its('stderr') { should match /HTTP\/1.1 200 OK/ }
 end
 describe command('curl -v https://www.github.com/') do
