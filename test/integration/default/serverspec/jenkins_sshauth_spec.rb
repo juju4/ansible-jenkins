1234ef3d1d02063158a65ade77319b42fbea6cb1
juju4 <juju4@users.noreply.github.com>
2018-12-20 16:38:30 -0500

    fix ssh key config for admin user - variable dfilepath now

--- test/integration/default/serverspec/jenkins_sshauth_spec.rb
+++ test/integration/default/serverspec/jenkins_sshauth_spec.rb
@@ -3,9 +3,15 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/var/lib/jenkins/users/admin/config.xml') do
+describe command('ls /var/lib/jenkins/users/admin*/config.xml') do
+  its(:stdout) { should match /config.xml/ }
+  its(:stderr) { should be_empty }
+  its(:exit_status) { should eq 0 }
+end
+
+adminxml_filepath = command('ls /var/lib/jenkins/users/admin*/config.xml').stdout.strip!
+describe file(adminxml_filepath) do
   it { should be_readable }
   its(:content) { should match /org.jenkinsci.main.modules.cli.auth.ssh.UserPropertyImpl/ }
   its(:content) { should match /jenkins-key-for-/ }
 end
-
